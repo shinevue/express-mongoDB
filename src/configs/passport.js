65106edc5c3c49a323717e17e5d3738aab765500
shine <shine401020@gmail.com>
2024-06-03 09:30:46 +0900

    fixed: add middleware

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -8,16 +8,17 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretOrKey;
 
-module.exports = passport => {
-    passport.use(
-        new JwtStrategy(opts, (jwt_payload, done) => {
-            Auth.findOne(jwt_payload.userId)
-                .then(user => {
-                    if(user) {
-                        return done(null, user)
-                    }
-                    return done(null, false)
-                }).catch(err => console.log(err))
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      Auth.findOne({ userId: jwt_payload.userId })
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
         })
-    )
+        .catch((err) => console.log(err));
+    })
+  );
 };
--- src/routes/articles.route.js
+++ src/routes/articles.route.js
@@ -7,12 +7,15 @@ const {
   updateAArticle,
   deleteAArticle,
 } = require("../controllers/articles.controller");
+const passport = require("passport");
 
-router.get("/articles-all", getAllArticles);
-router.post("/article/new", createAArticle);
-router.put("/article/:id", updateAArticle);
-router.delete("/article/:id", deleteAArticle);
-router.get("/article/:id", getAArticle);
-router.post("/articles-many", createManyArticles);
+const middleware = passport.authenticate("jwt", { session: false });
+
+router.get("/articles-all", middleware, getAllArticles);
+router.post("/article/new", middleware, createAArticle);
+router.put("/article/:id", middleware, updateAArticle);
+router.delete("/article/:id", middleware, deleteAArticle);
+router.get("/article/:id", middleware, getAArticle);
+router.post("/articles-many", middleware, createManyArticles);
 
 module.exports = router;
